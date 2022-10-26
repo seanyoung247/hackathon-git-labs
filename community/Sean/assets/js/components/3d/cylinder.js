0769ab8f9999735fb49810c9e72069a8f2595b33
Sean Young <seanyoung247@gmail.com>
2022-10-26 23:53:51 +0100

    feat: Adds initial cylinder primative

---
+++ community/Sean/assets/js/components/3d/cylinder.js
@@ -0,0 +1,75 @@
+
+import { createComponent, createTemplate } from '../../externals.js';
+import { WebPrimitive3D } from './primitive.js';
+
+(() => {
+
+    const styles = `
+        :host {
+            --radius: 50px;
+            --height: 100px;
+            --sides: 16;
+
+            --PI: 3.14159265358979;
+
+            height: var(--height);
+            width: calc(var(--radius) * 2);
+            transform-style: preserve-3d;
+        }
+        #faces {
+            width: 100%;
+            height: 100%;
+            position: relative;
+            transform-style: preserve-3d;
+        }
+        .face {
+            position: absolute;
+            left: 50%; top: 50%;
+            height: 100%;
+            width: calc( ( 2 * var(--PI) * var(--radius) ) / var(--sides) );
+
+            --a: calc( (360deg / var(--sides)) * var(--i) );
+
+            transform: translate(-50%,-50%) rotateY(var(--a)) translateZ(var(--radius));
+        }
+    `;
+    const html = `
+        <div id="faces">
+        </div>
+    `;
+
+    const generateFaces = count => {
+        const fragment = new DocumentFragment();
+        const faceTemp = document.createElement('div');
+        const slotTemp = document.createElement('slot');
+        faceTemp.classList.add('face');
+
+        for (let i = 1; i <= count; i++) {
+            const face = faceTemp.cloneNode();
+            const slot = slotTemp.cloneNode();
+            face.setAttribute('part', `face face-${i}`);
+            face.style.setProperty('--i', i);
+            slot.setAttribute('name', `face-${i}`);
+            face.append(slot);
+            fragment.append(face);
+        }
+        return fragment;
+    }
+
+    class Cylinder3D extends WebPrimitive3D {
+        static get tagName() { return 'cylinder-3d'; }
+        static get attributes() {
+            return {
+                'radius': {type: String, default: '50px'},
+                'height': {type: String, default: '100px'},
+                'sides': {type: Number, default: 16}
+            }
+        }
+        connectedCallback() {
+            this.shadowRoot.getElementById('faces').append(generateFaces(this._sides));
+        }
+    }
+
+    const template = createTemplate(html, styles);
+    createComponent(Cylinder3D, template);
+})();
--- community/Sean/assets/js/main.js
+++ community/Sean/assets/js/main.js
@@ -1,2 +1,3 @@
 import * as scene from './components/3d/scene.js';
 import * as cuboid from './components/3d/cuboid.js';
+import * as cylinder from './components/3d/cylinder.js';
