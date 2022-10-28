6c49e2c071a507575063d722729bf74e6429459d
Sean Young <seanyoung247@gmail.com>
2022-10-28 20:35:19 +0100

    refactor: Removes unnecessary closure

--- community/Sean/assets/js/components/3d/scene.js
+++ community/Sean/assets/js/components/3d/scene.js
@@ -1,27 +1,23 @@
 
 import { createComponent, createTemplate, WebComponent } from '../../externals.js';
 
-(() => {
-
-    const styles = `
-        slot {
-            display: flex;
-            perspective: 1500px;
-            transform-style: preserve-3d;
-        }
-    `;
-
-    const html = `<slot part="objects"></slot>`;
-
-    class Scene3D extends WebComponent {
-        static get tagName() { return 'scene-3d'; }
-        constructor() {
-            super();
-            this._createShadow({mode: 'open'});
-        }
+const styles = `
+    slot {
+        display: flex;
+        perspective: 1500px;
+        transform-style: preserve-3d;
     }
+`;
 
-    const template = createTemplate(html, styles);
-    createComponent(Scene3D, template);
+const html = `<slot part="objects"></slot>`;
 
-})();
+class Scene3D extends WebComponent {
+    static get tagName() { return 'scene-3d'; }
+    constructor() {
+        super();
+        this._createShadow({mode: 'open'});
+    }
+}
+
+const template = createTemplate(html, styles);
+createComponent(Scene3D, template);
