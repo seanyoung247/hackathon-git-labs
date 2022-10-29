a96edbf241de978caead06a2d37ff44b0742dad5
Sean Young <seanyoung247@gmail.com>
2022-10-29 01:23:10 +0100

    refactor: Consolidates Ngon code to dedicated class

--- community/Sean/assets/js/components/3d/cylinder.js
+++ community/Sean/assets/js/components/3d/cylinder.js
@@ -1,78 +1,29 @@
 
-import { createComponent, createTemplate, clamp } from '../../externals.js';
-import { WebPrimitive3D } from './primitive.js';
+import { clamp } from '../../externals.js';
+import { createRegularPolygon, RegularPolygon3D } from './primitive.js';
 
-const styles = `
-    :host {
-        --radius: 50px;
-        --height: 100px;
-        --sides: 16;
 
-        height: calc(var(--height) * 1em);
-        width: calc(var(--width) * 1em);
-        transform-style: preserve-3d;
-    }
-    #faces {
-        width: 100%;
-        height: 100%;
-        position: relative;
-        transform-style: preserve-3d;
-    }
-    .face {
-        position: absolute;
-        left: 50%; top: 50%;
-        height: 100%;
-        width: calc(var(--l) * 1em);
+createRegularPolygon(
+    class extends RegularPolygon3D {
+        static get tagName() { return 'cylinder-3d'; }
 
-        transform: 
-            translate(-50%,-50%)
-            rotateY( calc(var(--a) * var(--i)) )
-            translateZ( calc(var(--off) * 1em) );
-    }
-    .cap {
-        height: calc(var(--width) * 1em);
-        width: calc(var(--width) * 1em);
-        border-radius: 50%;
-    }
-    #top {
-        transform: 
-            translate(-50%,-50%) 
-            rotateX(90deg) 
-            translateZ( calc( (var(--height) / 2) * 1em) );
-    }
-    #bottom {
-        transform: 
-            translate(-50%,-50%) 
-            rotateX(-90deg) 
-            translateZ( calc( (var(--height) / 2) * 1em) );
-    }
-`;
-const html = `
-    <div id="faces">
-    </div>
-`;
+        static get attributes() {
+            return {
+                ...super.attributes,
+                'sides': {type: Number, default: 16}
+            }
+        }
 
-class Cylinder3D extends WebPrimitive3D {
-    static get tagName() { return 'cylinder-3d'; }
-
-    static get attributes() {
-        return {
-            ...super.attributes,
-            'sides': {type: Number, default: 16}
+        get sides() { return this._sides; }
+        set sides(val) {
+            if (val != this._sides) {
+                // Lets keep the number of sides sane
+                val = clamp(val, 8, 32);
+                this._sides = this.attributes.sides.type(val);
+                this._genFaces(this._faces);
+                this.setAttribute('sides', val);
+            }
         }
     }
+);
 
-    get sides() { return this._sides; }
-    set sides(val) {
-        if (val != this._sides) {
-            // Lets keep the number of sides sane
-            val = clamp(val, 8, 32);
-            this._sides = this.attributes.sides.type(val);
-            this._genFaces(this._faces);
-            this.setAttribute('sides', val);
-        }
-    }
-}
-
-const template = createTemplate(html, styles);
-createComponent(Cylinder3D, template);
--- community/Sean/assets/js/components/3d/primitive.js
+++ community/Sean/assets/js/components/3d/primitive.js
@@ -1,26 +1,5 @@
 
-import { WebComponent, createFragment } from '../../externals.js';
-
-
-const style = `
-    :host {
-        transform-style: preserve-3d;
-    }
-    #faces {
-        width: 100%;
-        height: 100%;
-        position: relative;
-        transform-style: preserve-3d;
-    }
-    .face {
-        position: absolute;
-        left: 50%; top: 50%;
-        height: 100%;
-    }
-`;
-
-
-const html = '<div id="faces"></div>';
+import { createTemplate, createComponent, WebComponent, createFragment } from '../../externals.js';
 
 
 export class WebPrimitive3D extends WebComponent {
@@ -34,29 +13,85 @@ export class WebPrimitive3D extends WebComponent {
         }
     }
 
-    static _createDefaultAccessor(self, attr, prop) {
-        Object.defineProperty(self, attr, {
-            get() { return self[prop]; },
-            set(val) {
-                self.style.setProperty(`--${attr}`, val);
-                self[prop] = self.attributes[attr].type(val);
-                self.setAttribute(attr, val);
-            }
-        });
-    }
-
     constructor() {
         super();
         this._createShadow({mode: 'open'});
         this._faces = this.shadowRoot.getElementById('faces');
     }
 
+    get sides() { return this._sides; }
+
+    get height() { return this._height; }
+    set height(val) {
+        this._height = this.attributes.height.type(val);
+        this._faces.style.setProperty('--height', `${this._height}${this._unit}`);
+        this.setAttribute('height', val);
+    }
+
+    get width() { return this._width; }
+    set width(val) {
+        this._width = this.attributes.width.type(val);
+        this._faces.style.setProperty('--width', `${this._width}${this._unit}`);
+        this.setAttribute('width', val);
+    }
+
     connectedCallback() {
-        this._faces.replaceChildren();
+        this._faces.style.setProperty('--height', `${this._height}${this._unit}`);
+        this._faces.style.setProperty('--width', `${this._width}${this._unit}`);
+    }
+}
+
+
+const style = `
+    :host {
+        transform-style: preserve-3d;
+    }
+    #faces {
+        position: relative;
+        width: var(--width);
+        height: var(--height);
+        transform-style: preserve-3d;
+    }
+    .face {
+        position: absolute;
+        left: 50%; top: 50%;
+        height: 100%;
+        width: var(--l);
+        transform:
+            translate(-50%,-50%)
+            rotateY(calc(var(--a) * var(--i)))
+            translateZ(var(--off));
+        backface-visibility: hidden;
+    }
+    .cap {
+        height: var(--width);
+        width: var(--width);
+        border-radius: 50%;
+    }
+    #top {
+        transform: 
+            translate(-50%,-50%) 
+            rotateX(90deg) 
+            translateZ( calc(var(--height) / 2) );
+    }
+    #bottom {
+        transform: 
+            translate(-50%,-50%) 
+            rotateX(-90deg) 
+            translateZ( calc(var(--height) / 2) );
+    }
+`;
+const html = '<div id="faces"></div>';
+
+
+export class RegularPolygon3D extends WebPrimitive3D {
+
+    connectedCallback() {
+        super.connectedCallback();
         this._genFaces(this._faces);
     }
 
-    /* NGon helpers */
+    /* Face properties and generation */
     _sideLength() {
         return this._width * Math.sin(Math.PI / this._sides);
     }
@@ -65,37 +100,39 @@ export class WebPrimitive3D extends WebComponent {
         return (this._width / 2) * Math.cos(Math.PI / this._sides);
     }
 
-    _genFace(id, cls='', i=0) {
+    _genFace(id, cls='', style=null) {
         return createFragment(
             `<div id="${id}" 
                 part="face ${id}" 
-                class="face ${cls}" 
-                ${i ? `style="--i: ${i}"` : ''}>
+                class="face ${cls}"
+                ${style ? `style="${style}"` : ''}>
                 <slot name="${id}"></slot>
             </div>`, 
         );
     }
 
     _genFaces(container) {
+        const a = (Math.PI * 2) / this._sides; 
         const l = this._sideLength();
-        const a = (Math.PI*2) / this._sides; 
-        const offset = this._faceOffset();
+        const off = this._faceOffset();
 
         const fragment = new DocumentFragment();
 
-        // Pass shared face properties to faces container
-        container.style.setProperty('--a', `${a}rad`);// face angles
-        container.style.setProperty('--l', l);          // face length
-        container.style.setProperty('--off', offset);   // face offsets
-
+        this._faces.style.setProperty('--a', `${a}rad`);
+        this._faces.style.setProperty('--l', `${l}${this._unit}`);
+        this._faces.style.setProperty('--off', `${off}${this._unit}`);
         // Generate sides
         for (let i = 1; i <= this._sides; i++) {
-            fragment.append(this._genFace(`face-${i}`, '', i));
+            fragment.append(this._genFace(`face-${i}`, '', `--i:${i}`));
         }
         // Generate caps
         fragment.append(this._genFace('top', 'cap'));
         fragment.append(this._genFace('bottom', 'cap'));
-        container.replaceChildren();
-        container.append(fragment);
+        container.replaceChildren(fragment);
     }
+}
+
+export const createRegularPolygon = (poly) => {
+    const template = createTemplate(html, style);
+    createComponent(poly, template);
 }
